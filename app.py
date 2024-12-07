dd8b90efde20571677dbb45e64fbfd5ecb598244
rohitpotluri <rohitpotluri@my.unt.edu>
2024-12-07 01:14:55 -0600

    prevention off retrieval error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import RetrievalQA
+from langchain.chains import create_retrieval_chain
 from config import GROQ_API
 from embeddings import vectorstore
 from prompt import get_prompt_template
@@ -22,19 +22,19 @@ if st.button("Generate Embeddings"):
 if user_query:
     document_chain = create_stuff_documents_chain(llm, get_prompt_template())
     retriever = vectorstore.as_retriever()
-    retrieval_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
     with st.spinner("Retrieving and processing your query..."):
         start_time = time.process_time()
-        response = retrieval_chain.invoke({'query': user_query})
+        response = retrieval_chain.invoke({'input': user_query})
         end_time = time.process_time() - start_time
 
         st.subheader("Answer:")
-        st.write(response['result'])
+        st.write(response['answer'])
 
         st.write(f"Response time: {end_time:.2f} seconds")
 
         with st.expander("Relevant Document Chunks"):
-            for i, doc in enumerate(response["source_documents"]):
+            for i, doc in enumerate(response["context"]):
                 st.write(f"Chunk {i + 1}: {doc.page_content}")
                 st.write("---")
